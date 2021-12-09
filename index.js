74acff0375e77181f2e9e2481503f6612f4f06e8
Juan David Franco <jfrancoh5@gmail.com>
2021-12-09 14:10:46 -0500

    boton de hamburguesa

---
+++ index.js
@@ -0,0 +1,7 @@
+let burguer_button = document.getElementById('burguer_button');
+
+function burguerButton(x){
+    burguer_button.classList.toggle('change_burguer-button');
+    x.classList.toggle('change');
+}
+
