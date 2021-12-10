b179057c07a7b40f1f12a5659d0c01df65de63c8
Juan Franco <jfrancoh5@gmail.com>
2021-12-09 22:14:59 -0500

    triunfo boton de hamburguesa

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 let burguer_button = document.getElementById('burguer_button');
+let headerBurguer = document.getElementById('header__burguer');
 
 function burguerButton(x){
     burguer_button.classList.toggle('change_burguer-button');
     x.classList.toggle('change');
+    console.log(burguer_button.classList.toggle('change_burguer-button'))
+    if(burguer_button.classList.toggle('change_burguer-button') !== false){
+        header__burguer.style.transform = `translateY(100vh)`;
+        header__burguer.style.transition = 'transform 700ms'; 
+    }else{
+        header__burguer.style.transform = `translateY(0vh)`;
+        header__burguer.style.transition = 'transform 700ms';
+    }
 }
-
